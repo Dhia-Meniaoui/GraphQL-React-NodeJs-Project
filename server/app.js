318ae10bab2ef1dbf9a975649de91c71c3e13280
dhia <diaminiaoui@gmail.com>
2022-01-23 23:57:24 +0100

    23/01

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
-
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 
 
 app.use('/graphql',graphqlHTTP({
-
-
+    schema,
+    graphiql : true
 }));
 
 
---
+++ server/schema/schema.js
@@ -0,0 +1,54 @@
+const graphql= require('graphql');
+const {
+    GraphQLObjectType ,
+    GraphQLString ,
+    GraphQLSchema ,
+    GraphQLID,
+    GraphQLInt
+    }= graphql;
+const _= require('lodash');
+
+const BookType = new GraphQLObjectType({
+    name:'Book',
+    fields: () => ({
+        id: { type: GraphQLID},
+        name: { type: GraphQLString},
+        genre: { type: GraphQLString}
+    })
+});
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () =>({
+        id: {type : GraphQLID},
+        name : {type : GraphQLString},
+        age : {type: GraphQLInt}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        book:{
+            type:BookType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                //code to get data from db 
+                return _.find(books , {id: args.id});
+
+            }
+        },
+        author:{
+            type:AuthorType,
+            args: { id: {type: GraphQLID}},
+            resolve(parent , args ){
+                return _.find(author , {id: args.id})
+            }
+        }
+    }
+})
+
+module.exports= new GraphQLSchema({
+    query: RootQuery
+})
